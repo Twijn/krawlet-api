251512eb30e644a1a45ae1070c33c3296b26649f
Tyler Twining <twijn@twijn.net>
2025-08-25 00:13:39 -0500

    feat: Add HA (high-availability) transaction reporting

--- src/kromerWs/index.ts
+++ src/kromerWs/index.ts
@@ -3,9 +3,21 @@ import playerManager from "../lib/managers/playerManager";
 import {rcc} from "../chat";
 
 import {hook} from "../lib/webhook";
-import {Transaction} from "kromer";
+import {Transaction, TransactionWithMeta} from "kromer";
+import {HATransactions} from "../lib/HATransactions";
 
 const STRIPPED_META_ENTRIES = ["error", "message", "return"];
+
+interface TransactionData {
+    from: string;
+    to: string;
+    entries: {
+        error?: string;
+        message?: string;
+    }
+}
+
+type Handler = (transaction: TransactionWithMeta, data: TransactionData) => void;
 
 function transactionUrl(transaction: Transaction) {
     return `[#${transaction.id}](https://kromer.club/transactions/${transaction.id})`;
@@ -20,21 +32,64 @@ function addressUrl(address: string, label?: string) {
     return `[${label}](https://kromer.club/addresses/${address})`;
 }
 
-const client = kromer.createWsClient(undefined, [
-    "transactions",
-]);
+function sendDiscordMessage(transaction: TransactionWithMeta, data: TransactionData) {
+    let metadata = "";
 
-client.on("ready", () => {
-    console.log("Connected to Kromer WS!");
-});
+    if (data.entries.error) {
+        metadata += `\n:x: *${data.entries.error}*`;
+    }
+    if (data.entries.message) {
+        metadata += `\n:speech_balloon: *${data.entries.message}*`;
+    }
 
-client.on("error", err => {
-    console.error(err);
-});
+    let strippedEntries = transaction?.meta?.entries ?
+        transaction.meta.entries.filter(x => !STRIPPED_META_ENTRIES.includes(x.name.toLowerCase())) : [];
+    if (strippedEntries.length > 0) {
+        metadata += "\n`" + `${strippedEntries.map(x => `${x.name}${x.value ? `=${x.value}` : ""}`).join(";")}`
+            .replace(/`/g, "\\`") + "`";
+    }
 
-client.on("transaction", transaction => {
+    hook.batchedSend(`${transactionUrl(transaction)} | ${transaction.from ? addressUrl(transaction.from, data.from) : "unknown"} -> ${addressUrl(transaction.to, data.to)} | ${transaction.value.toFixed(2)} KRO${metadata}`).catch(console.error);
+}
+
+function sendInGameMessage(transaction: TransactionWithMeta, data: TransactionData) {
     let sentNames: string[] = [];
+    let message = "";
 
+    if (data.entries.error) {
+        message = `<dark_red>Error:</dark_red> <red>${data.entries.error}</red>`;
+    } else if (data.entries.message) {
+        message = `<blue>Message:</blue> <gray>${data.entries.message}</gray>`;
+    }
+
+    playerManager.getNotifiedPlayers().forEach(player => {
+        const fromSelf = transaction.from === player.kromerAddress;
+        const toSelf = transaction.to === player.kromerAddress;
+        if (player.notifications === "all" ||
+            (player.notifications === "self" && (
+                fromSelf || toSelf
+            ))) {
+
+            rcc.tell(player.minecraftName, `<gray>New transaction:</gray>\n ${data.from} <gray>-></gray> ` +
+                `${data.to} <gray>|</gray> ` +
+                `${transaction.value.toFixed(2)} <gray>KRO</gray> ${message}`.trim()).catch(console.error);
+            sentNames.push(player.minecraftName);
+        }
+    });
+
+    if (sentNames.length > 0) {
+        console.log(`Sent transaction (${transaction.id}) notifications to ${sentNames.join(", ")}`);
+    }
+}
+
+const handlers: Handler[] = [
+    sendDiscordMessage,
+    sendInGameMessage,
+];
+
+const haTransactions = new HATransactions(kromer);
+
+haTransactions.on((transaction: TransactionWithMeta) => {
     let from = transaction.from ?? "unknown";
     let to = transaction.to;
 
@@ -48,57 +103,26 @@ client.on("transaction", transaction => {
         to = toPlayer?.minecraftName;
     }
 
-    let message = "";
-
     const errorEntry = transaction.meta?.entries
-        .find(x => x.name.toLowerCase() === "error");
+        ?.find(x => x.name.toLowerCase() === "error");
     const messageEntry = transaction.meta?.entries
-        .find(x => x.name.toLowerCase() === "message");
+        ?.find(x => x.name.toLowerCase() === "message");
 
-    let metadata = "";
-
-    if (errorEntry) {
-        metadata += `\n:x: *${errorEntry.value}*`;
-    }
-    if (messageEntry) {
-        metadata += `\n:speech_balloon: *${messageEntry.value}*`;
-    }
-
-    let strippedEntries = transaction?.meta?.entries ?
-        transaction.meta.entries.filter(x => !STRIPPED_META_ENTRIES.includes(x.name.toLowerCase())) : [];
-    if (strippedEntries.length > 0) {
-        metadata += "\n`" + `${strippedEntries.map(x => `${x.name}${x.value ? `=${x.value}` : ""}`).join(";")}`
-            .replace(/`/g, "\\`") + "`";
+    const data: TransactionData = {
+        from, to,
+        entries: {
+            error: errorEntry?.value,
+            message: messageEntry?.value,
+        }
     }
 
-    hook.batchedSend(`${transactionUrl(transaction)} | ${transaction.from ? addressUrl(transaction.from, from) : "unknown"} -> ${addressUrl(transaction.to, to)} | ${transaction.value.toFixed(2)} KRO${metadata}`).catch(console.error);
-
-    playerManager.getNotifiedPlayers().forEach(player => {
-        const fromSelf = transaction.from === player.kromerAddress;
-        const toSelf = transaction.to === player.kromerAddress;
-        if (player.notifications === "all" ||
-            (player.notifications === "self" && (
-                fromSelf || toSelf
-            ))) {
-
-            if (errorEntry) {
-                message = `<dark_red>Error:</dark_red> <red>${errorEntry.value}</red>`;
-            } else if (messageEntry) {
-                message = `<blue>Message:</blue> <gray>${messageEntry.value}</gray>`;
-            }
-
-            rcc.tell(player.minecraftName, `<gray>New transaction:</gray>\n ${from} <gray>-></gray> ` +
-                `${to} <gray>|</gray> ` +
-                `${transaction.value.toFixed(2)} <gray>KRO</gray> ${message}`.trim()).catch(console.error);
-            sentNames.push(player.minecraftName);
+    handlers.forEach(handler => {
+        try {
+            handler(transaction, data);
+        } catch (error) {
+            console.error('Error in transaction handler:', error);
         }
     });
-
-    if (sentNames.length > 0) {
-        console.log(`Sent transaction (${transaction.id}) notifications to ${sentNames.join(", ")}`);
-    }
 });
 
-client.connect().catch(console.error);
-
-export default client;
+export default haTransactions;
