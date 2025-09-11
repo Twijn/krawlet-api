6fb97dd90cb1a7399e718194b66201fb7cd74bbb
Tyler Twining <twijn@twijn.net>
2025-09-11 00:35:09 -0500

    chore: lint-staged test

--- src/lib/webhook.ts
+++ src/lib/webhook.ts
@@ -4,3 +4,4 @@ export const hook = new DiscordWebhook(process.env.KROMER_WEBHOOK!, {
     avatarURL: "https://www.kromer.club/images/verified/serverwelf.png",
     username: "Kromer Logs"
 });
+// lint-staged test
